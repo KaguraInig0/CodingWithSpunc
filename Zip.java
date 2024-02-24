c86c34fa532a85874b85f44a665b503c17febfcf
Inigo Dela Vega <143131551+KaguraInig0@users.noreply.github.com>
2024-02-24 02:32:42 -0800

    Added working prototype

--- Zip.java
+++ Zip.java
@@ -2,14 +2,16 @@ public class Zip {
     
     public static void main(String[]args){
         
-        string[] furniture = {table, chair, rack, shelf};
+        String[] furniture = {"table", "chair", "rack", "shelf"};
         int[] price = {100, 50, 80, 40};
+        zip(furniture, price);
 
     }
 
-    static void zip(){
-        for(int i=0; i<0; i++){
-            
+    public static void zip(String[] a, int[] b){
+        for(int i=0; i<a.length; i++){
+            System.out.println(a[i]);
+            System.out.println(b[i]);
         }
     }
 
